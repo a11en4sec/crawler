05be37e6b05fa7b544b982c5b4ed03a4f4f65c60
a11en4sec <a11en4sec@gmail.com>
2022-11-26 20:25:10 +0800

    encoding xxx -> utf8

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/a11en4sec/crawler
 
 go 1.18
+
+require (
+	golang.org/x/net v0.2.0
+	golang.org/x/text v0.4.0
+)
--- main.go
+++ main.go
@@ -1,33 +1,61 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 	url := "https://www.thepaper.cn/"
+
+	body, err := Fetch(url)
+	if err != nil {
+		fmt.Printf("read content failed:%v", err)
+		return
+	}
+
+	fmt.Println(string(body))
+}
+
+func Fetch(url string) ([]byte, error) {
+
 	resp, err := http.Get(url)
 
 	if err != nil {
-		fmt.Println("fetch url error:%v", err)
-		return
+		panic(err)
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%v", resp.StatusCode)
-		return
+		fmt.Printf("Error status code :%d", resp.StatusCode)
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+
+	return ioutil.ReadAll(utf8Reader)
+
+}
+
+// DeterminEncoding 检测网页的字符编码并将文本统一转换为 UTF-8 格式
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
 
 	if err != nil {
-		fmt.Println("read content failed:%v", err)
-		return
+		fmt.Printf("fetch error :%v", err)
+		return unicode.UTF8
 	}
 
-	fmt.Println("body:", string(body))
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+
+	return e
 }
