b3682424076b35f7c1439811b5a0bf8ebcf79ff4
a11en4sec <a11en4sec@gmail.com>
2022-12-18 08:00:46 +0800

    增加优先级队列

--- engine/schedule.go
+++ engine/schedule.go
@@ -1,7 +1,6 @@
 package engine
 
 import (
-	"fmt"
 	"github.com/a11en4sec/crawler/collect"
 	"go.uber.org/zap"
 )
@@ -14,12 +13,13 @@ type Scheduler interface {
 
 // Schedule 实现了接口Scheduler
 type Schedule struct {
-	requestCh chan *collect.Request
-	workerCh  chan *collect.Request
+	requestCh   chan *collect.Request
+	workerCh    chan *collect.Request
+	priReqQueue []*collect.Request // 优先级队列
+	reqQueue    []*collect.Request
+	Logger      *zap.Logger
 	//out       chan collect.ParseResult
 	//options
-	reqQueue []*collect.Request
-	Logger   *zap.Logger
 }
 
 func NewSchedule() *Schedule {
@@ -51,25 +51,44 @@ func (s *Schedule) Output() *collect.Request {
 }
 
 func (s *Schedule) Schedule() {
-	go func() {
-		for {
-			var req *collect.Request
-			var ch chan *collect.Request
-			//ch := make(chan *collect.Request)
+	// 初始化不能放在协程中,否在会丢失请求
+	var req *collect.Request
+	var workerCh chan *collect.Request
+	//ch := make(chan *collect.Request)
 
-			if len(s.reqQueue) > 0 {
-				req = s.reqQueue[0]
-				s.reqQueue = s.reqQueue[1:]
-				ch = s.workerCh
-			}
+	//go func() {
+	for {
+		//var req *collect.Request
+		//var ch chan *collect.Request
+		//ch := make(chan *collect.Request)
 
-			select {
-			case r := <-s.requestCh: // 监听一次fetch的解析结果中是否有新的request加入
-				s.reqQueue = append(s.reqQueue, r)
-			case ch <- req: // 传递给workerCh
-				fmt.Println("123")
-			}
+		// 初始时，并且优先级队列不为空
+		if req == nil && len(s.priReqQueue) > 0 {
+			req = s.priReqQueue[0]
+			s.priReqQueue = s.priReqQueue[1:]
+			workerCh = s.workerCh
 		}
-	}()
+		// 初始时，并且普通队列不为空
+		if req == nil && len(s.reqQueue) > 0 {
+			req = s.reqQueue[0]
+			s.reqQueue = s.reqQueue[1:]
+			workerCh = s.workerCh
+		}
+
+		select {
+		case r := <-s.requestCh: // 监听一次fetch的解析结果中是否有新的request加入
+			//s.reqQueue = append(s.reqQueue, r)
+			if r.Priority > 0 {
+				s.priReqQueue = append(s.priReqQueue, r)
+			} else {
+				s.reqQueue = append(s.reqQueue, r)
+			}
+		case workerCh <- req: // 传递给workerCh
+			//fmt.Println("123")
+			req = nil
+			workerCh = nil
+		}
+	}
+	//}()
 
 }
